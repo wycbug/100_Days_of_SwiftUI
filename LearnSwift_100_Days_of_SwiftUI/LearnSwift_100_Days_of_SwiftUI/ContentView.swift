d1c62cbb09a447510d78ea274a4b75d2ca8a5221
wangyicheng02 <wangyicheng02@baidu.com>
2023-06-06 19:27:50 +0800

    init project

---
+++ LearnSwift_100_Days_of_SwiftUI/LearnSwift_100_Days_of_SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearnSwift_100_Days_of_SwiftUI
+//
+//  Created by wangyichengwork on 2023/6/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnSwift_100_Days_of_SwiftUI/LearnSwift_100_Days_of_SwiftUI/LearnSwift_100_Days_of_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwift_100_Days_of_SwiftUIApp.swift
+//  LearnSwift_100_Days_of_SwiftUI
+//
+//  Created by wangyichengwork on 2023/6/6.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwift_100_Days_of_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
